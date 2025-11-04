3c60d4533191311d54e4bc181b6d79a13007fb82
Aaron Sulwer <asulwer@gmail.com>
2025-11-04 13:34:05 -0700

    added logging

--- 2wire.py
+++ 2wire.py
@@ -1,12 +1,14 @@
 import asyncio
 from gpiozero.pins.lgpio import LGPIOFactory
 from gpiozero import Device, Button
+import logging
+import logging.config
 
 async def button_pressed_handler():
-    print("Button pressed asynchronously!")
+    logging.info("Button pressed asynchronously!")
 
 async def button_released_handler():
-    print("Button released asynchronously!")
+    logging.info("Button released asynchronously!")
 
 async def main():
     button = Button(26)
@@ -14,12 +16,16 @@ async def main():
     button.when_pressed = lambda: loop.call_soon_threadsafe(asyncio.create_task, button_pressed_handler())
     button.when_released = lambda: loop.call_soon_threadsafe(asyncio.create_task, button_released_handler())
 
-    print("Waiting for button presses...")
+    logging.info("Waiting for button presses...")
     await asyncio.Future()
 
 if __name__ == '__main__':
+    logging.config.fileConfig('logging.conf')
+    logging.info("starting 2wire")
+
     try:
+        logging.info("use pin factory LGPIO")
         Device.pin_factory = LGPIOFactory()
         asyncio.run(main())
     except Exception as e:
-        print(e)
+        logging.error(e)
--- webserver.py
+++ webserver.py
@@ -1,5 +1,10 @@
 import flaskr
+import logging
+import logging.config
 
 if __name__ == '__main__':
+    logging.config.fileConfig('logging.conf')
+
+    logging.info("starting webserver")
     app = flaskr.create_app()
     flaskr.socketio.run(app, host='0.0.0.0', port=5000, debug=True)
